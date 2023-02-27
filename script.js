7498ed206423567e862b3bd6a96b819e1d113397
Dan Dudaiev <roffrooff@gmail.com>
2023-02-27 15:44:33 +0200

    Added first practice scripts

---
+++ script.js
@@ -0,0 +1,24 @@
+// const result = 108 + 223 - 2 ** 5;
+// console.log(result);
+
+// Склади фразу за допомогою шаблонних рядків A has B bots in stock, де A, B - змінні вставлені в рядок.
+
+// const companyName = 'Cyberdyne Systems';
+// const repairBots = 150;
+// const defenceBots = 50;
+// const message = `${companyName} has ${repairBots + defenceBots} bots in stock.`;
+// console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+
+// Напиши скрипт, який розраховує індекс маси тіла людини. Для цього необхідно розділити вагу в кілограмах на квадрат висоти людини у метрах.
+
+// Вага та висота зберігаються у змінних weight та height, але не як числа, а в вигляді рядків (спеціально для завдання). Не цілі числа можуть бути задані у вигляді 24.7 або 24,7, тобто як роздільник дробової частини може бути кома.
+
+// Індекс маси тіла необхідно округлити до однієї цифри після коми;
+
+// let weight = '88,3';
+// let height = '1.75';
+
+// const message = "Your body mass index is: "
+// const bmi = Number(weight.replace(",", ".")) / (Number(height.replace(",", ".")) ** 2);
+
+// console.log(message + bmi.toFixed(1)); // 28.8
